71f75ca808cfcf772be81144b3182f1361587cc5
Brian Mulindi <mulindijrn@gmail.com>
2023-09-20 18:42:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  if (customerName) {
+    customerName = customerName.toUpperCase();
+  }
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const LeastFavoriteCustomer = 'bob'
+
+function changeLeastFavoriteCustomer(){
+    LeastFavoriteCustomer  = 'Crytal'
+}
+
+
+
